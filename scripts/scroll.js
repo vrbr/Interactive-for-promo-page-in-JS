c6b30ad7559b4d05b8312e7a467d4e8ad07ef57b
vrbr <lpvamaria@gmail.com>
2022-01-22 13:53:52 +0200

    Инкапсуляция scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,19 +1,26 @@
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a');
+    const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
 
-seamless.polyfill();
+    const newArray = [...links, linkCharacteristics];
 
-links.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-    
-        const id = link.getAttribute('href').substring(1);
-        const section = document.getElementById(id);
+    seamless.polyfill();
+
+    newArray.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
         
-        if (section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: 'smooth',
-                block: 'start'
-            })
-        }
+            const id = link.getAttribute('href').substring(1);
+            const section = document.getElementById(id);
+            
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            }
+        })
     })
-})
+}
+
+scrollFunc();
